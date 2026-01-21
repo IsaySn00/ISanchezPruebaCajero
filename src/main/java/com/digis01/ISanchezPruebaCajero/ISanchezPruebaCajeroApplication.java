56dbb76cd1da1078a2695bb964dee3f53dead52d
Alien 1 <Alien 1@192.167.0.65>
2026-01-20 18:22:45 -0600

    Primer commit (con conexion a base de datos)

---
+++ src/main/java/com/digis01/ISanchezPruebaCajero/ISanchezPruebaCajeroApplication.java
@@ -0,0 +1,13 @@
+package com.digis01.ISanchezPruebaCajero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ISanchezPruebaCajeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ISanchezPruebaCajeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digis01/ISanchezPruebaCajero/ISanchezPruebaCajeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digis01.ISanchezPruebaCajero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ISanchezPruebaCajeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
